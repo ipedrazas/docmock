a4690783ab1c7d32df814870fc73f087210192da
Ivan Pedrazas <ipedrazas@gmail.com>
2016-07-22 10:12:34 +0100

    cleanup

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
-from flask import Flask, jsonify
-import os
 import json
-import string
 import random
-from random import randint
+import string
+
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
-SIZE=12
+SIZE = 12
+
 
 def get_rnd_string():
-    chars=string.ascii_uppercase + string.digits
+    chars = string.ascii_uppercase + string.digits
     return ''.join(random.choice(chars) for _ in range(SIZE))
 
 
 def get_rnd_number():
-    return randint(0, 2048)
+    return random.randint(0, 2048)
 
 
 def get_rnd_bool():
     return bool(random.getrandbits(1))
 
 
-
 def generate_object(obj_schema):
     rnd_obj = {}
     for key in obj_schema:
         elem = obj_schema[key]
-        app.logger.debug(elem['type'])
         if elem['type'] == 'string':
             rnd_obj[key] = get_rnd_string()
         if elem['type'] == 'number':
@@ -40,18 +38,16 @@ def generate_object(obj_schema):
             rnd_obj[key] = generate_object(elem['schema'])
         if elem['type'] == 'list':
             rnd_obj[key] = generate_collection(elem['schema'])
-
-    app.logger.debug(rnd_obj)
     return rnd_obj
-        # print "key: %s , value: %s" % (key, mydictionary[key])
 
 
 def generate_collection(obj_schema):
-    num_objs = randint(0, 20)
+    num_objs = random.randint(0, 20)
     col = []
-    for x in range(1,num_objs):
+    for x in range(1, num_objs):
         col.append(generate_object(obj_schema))
     return col
+
 
 def load_schema():
     with open('/data/schema.json') as data_file:
@@ -60,8 +56,7 @@ def load_schema():
 
 @app.route('/', methods=['GET'])
 def hello():
-    schema = load_schema()
-    return jsonify(generate_collection(schema))
+    return jsonify(generate_collection(load_schema()))
 
 
 if __name__ == '__main__':
